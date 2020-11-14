6c12133a7ccd0a3afd1bf8dea0af3d4a3bb2b9f0
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-11-14 09:02:48 +0000

    add clojurescript configuration

--- project.clj
+++ project.clj
@@ -3,11 +3,26 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.773"]     ; based on google compiler
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [ring/ring-json "0.5.0"]]
-  :plugins [[lein-ring "0.12.5"]]
+                 [ring/ring-json "0.5.0"]
+                 [http-kit "2.3.0"]]
+  :plugins [[lein-ring "0.12.5"]
+            [lein-cljsbuild "1.1.8"]]
+  :cljsbuild {
+              :builds [{
+                        ; The path to the top-level ClojureScript source directory:
+                        :source-paths ["src-cljs"]
+                        ; The standard ClojureScript compiler options:
+                        ; (See the ClojureScript compiler documentation for details.)
+                        :compiler {
+                                   :output-to "war/javascripts/main.js"  ; default: target/cljsbuild-main.js
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]}
   :ring {:handler clj-cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        [ring/ring-mock "0.3.2"]
+                        [org.clojure/clojurescript "1.9.521"]]}}
+  )
