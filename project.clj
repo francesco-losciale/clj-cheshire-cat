2c8f546034ef8ad0044e05727303b0f588abcef2
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-11-14 08:11:52 +0000

    introduce ring-json and replace cheshire.core library that we used to serialise the map to json

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.5.0"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler clj-cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]
-                        [cheshire "5.4.0"]]}})
+                        [ring/ring-mock "0.3.2"]]}})
