6bc3ad424b63104e5bcb98cdf581732fdb13516c
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-11-14 16:17:16 +0000

    add enfocus and use it to display the content of the backend service in the dom elements

--- src-cljs/cheshire/core.cljs
+++ src-cljs/cheshire/core.cljs
@@ -2,11 +2,14 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [clojure.browser.repl :as repl]
             [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]]))
+            [cljs.core.async :refer [<!]]
+            [enfocus.core :as ef]))
 
 ; ^:export is metadata that allows you to annotate functions and pass additional information to the compiler
 (defn ^:export init []
   (repl/connect "http://localhost:9000/repl")
   (go
-    (let [response (<! (http/get "/cheshire-cat"))]
-      (js/alert (:body response)))))
+    (let [response (<! (http/get "/cheshire-cat"))
+          body (:body response)]
+      (ef/at "#cat-name" (ef/content (:name body)))
+      (ef/at "#status" (ef/content (:status body))))))
