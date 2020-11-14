e6cb397916dbd3f2988fb9ccd6c62148033ec61d
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-11-14 16:01:39 +0000

    add cljs-http and use it to ping the backend service

--- src-cljs/cheshire/core.cljs
+++ src-cljs/cheshire/core.cljs
@@ -1,6 +1,12 @@
 (ns cheshire.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [clojure.browser.repl :as repl]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
 
 ; ^:export is metadata that allows you to annotate functions and pass additional information to the compiler
 (defn ^:export init []
-  (repl/connect "http://localhost:9000/repl"))
+  (repl/connect "http://localhost:9000/repl")
+  (go
+    (let [response (<! (http/get "/cheshire-cat"))]
+      (js/alert (:body response)))))
