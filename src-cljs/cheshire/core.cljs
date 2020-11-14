113e977334421bba4b44a5bb3577d3a317e53859
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-11-14 16:28:52 +0000

    added enfocus event listening and effects

--- src-cljs/cheshire/core.cljs
+++ src-cljs/cheshire/core.cljs
@@ -3,7 +3,9 @@
   (:require [clojure.browser.repl :as repl]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
-            [enfocus.core :as ef]))
+            [enfocus.core :as ef]
+            [enfocus.events :as ev]
+            [enfocus.effects :as ee]))
 
 ; ^:export is metadata that allows you to annotate functions and pass additional information to the compiler
 (defn ^:export init []
@@ -14,4 +16,13 @@
       (ef/at "#cat-name" (ef/content (:name body))
              "#status" (ef/do->
                          (ef/content (:status body))
-                         (ef/set-style :font-size "500%"))))))
+                         (ef/set-style :font-size "500%")))
+      (ef/at "#button1" (ev/listen
+                          :click
+                          say-goodbye)))))
+
+(defn say-goodbye []
+  (ef/at
+    "#cat-name" (ee/fade-out 500)
+    "#button1" (ee/fade-out 500)
+    "#status" (ee/fade-out 5000)))
